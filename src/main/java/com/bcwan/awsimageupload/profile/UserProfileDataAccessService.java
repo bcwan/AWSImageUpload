3e2886020ca7476f5a73141c4617791bd181d312
bcwan <bcwan8@gmail.com>
2020-11-25 00:27:15 -0800

    Set up fake database injection

--- src/main/java/com/bcwan/awsimageupload/profile/UserProfileDataAccessService.java
+++ src/main/java/com/bcwan/awsimageupload/profile/UserProfileDataAccessService.java
@@ -1,9 +1,25 @@
 package com.bcwan.awsimageupload.profile;
 
+import java.util.List;
+
+import com.bcwan.awsimageupload.datastore.FakeUserProfileDataStore;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 // this class interacts with the database directly
 @Repository
 public class UserProfileDataAccessService {
   
+  // you can use this class (converted as interface) and switch databases to PostgreSQL or MySQL
+  private final FakeUserProfileDataStore fakeUserProfileDataStore;
+
+  @Autowired
+  public UserProfileDataAccessService(FakeUserProfileDataStore fakeUserProfileDataStore) {
+    this.fakeUserProfileDataStore = fakeUserProfileDataStore;
+  }
+
+  List<UserProfile> getUserProfiles() {
+    return fakeUserProfileDataStore.getUserProfiles();
+  }
 }
