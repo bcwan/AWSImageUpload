af679deda430f92c1ef4b070e3fee0fbd178d34f
bcwan <bcwan8@gmail.com>
2020-11-25 00:33:03 -0800

    Set up UserProfileService to connect with fake database

--- src/main/java/com/bcwan/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/bcwan/awsimageupload/profile/UserProfileService.java
@@ -1,5 +1,19 @@
 package com.bcwan.awsimageupload.profile;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserProfileService {
   
+  private final UserProfileDataAccessService userProfileDataAccessService;
+
+  @Autowired
+  public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    this.userProfileDataAccessService = userProfileDataAccessService;
+  }
+
+  List<UserProfile> getUserProfiles() {
+    return userProfileDataAccessService.getUserProfiles();
+  }
 }
