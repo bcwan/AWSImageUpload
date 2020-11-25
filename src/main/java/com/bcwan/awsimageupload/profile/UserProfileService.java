42689ee7d39fea8ab0eed12fa6b577120cda7769
bcwan <bcwan8@gmail.com>
2020-11-25 00:38:09 -0800

    Add UserProfileService connection to UserProfileController

--- src/main/java/com/bcwan/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/bcwan/awsimageupload/profile/UserProfileService.java
@@ -1,5 +1,7 @@
 package com.bcwan.awsimageupload.profile;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
