95a642d3d16a17ff8aef8f1730fb9dabed180275
bcwan <bcwan8@gmail.com>
2020-11-24 22:29:49 -0800

    Add getter and setter methods for UserProfile

--- src/main/java/com/bcwan/awsimageupload/profile/UserProfile.java
+++ src/main/java/com/bcwan/awsimageupload/profile/UserProfile.java
@@ -3,7 +3,7 @@ package com.bcwan.awsimageupload.profile;
 import java.util.UUID;
 
 public class UserProfile {
-  
+
   private final UUID userProfileId;
   private final String username;
   private String userProfileImageLink; // for S3 key
@@ -11,6 +11,22 @@ public class UserProfile {
   public UserProfile(UUID userProfileId, String username, String userProfileImageLink) {
     this.userProfileId = userProfileId;
     this.username = username;
+    this.setUserProfileImageLink(userProfileImageLink);
+  }
+
+  public UUID getUserProfileId() {
+    return userProfileId;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getUserProfileImageLink() {
+    return userProfileImageLink;
+  }
+
+  public void setUserProfileImageLink(String userProfileImageLink) {
     this.userProfileImageLink = userProfileImageLink;
   }
   
