c3bb040608ef344f8cc6b3b73bf30d42494ac6f3
bcwan <bcwan8@gmail.com>
2020-11-25 00:21:33 -0800

    Set up UserProfileDataAccessService class to interact with database

---
+++ src/main/java/com/bcwan/awsimageupload/profile/UserProfileDataAccessService.java
@@ -0,0 +1,9 @@
+package com.bcwan.awsimageupload.profile;
+
+import org.springframework.stereotype.Repository;
+
+// this class interacts with the database directly
+@Repository
+public class UserProfileDataAccessService {
+  
+}
---
+++ src/main/java/com/bcwan/awsimageupload/profile/UserProfileService.java
@@ -0,0 +1,5 @@
+package com.bcwan.awsimageupload.profile;
+
+public class UserProfileService {
+  
+}
