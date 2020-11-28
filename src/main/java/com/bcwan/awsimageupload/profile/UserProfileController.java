31e9dc36cb0231bbe15570b967521c794187cb5c
bcwan <bcwan8@gmail.com>
2020-11-27 17:38:55 -0800

    Add comments for UserProfileController

--- src/main/java/com/bcwan/awsimageupload/profile/UserProfileController.java
+++ src/main/java/com/bcwan/awsimageupload/profile/UserProfileController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// route for user profiles
 @RestController
 @RequestMapping("api/v1/user-profile")
 public class UserProfileController {
